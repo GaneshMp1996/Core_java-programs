e217982a398b2e8069e970ca0142fd69c570b554
ganeshmp1996 <[ganeshmpganesh3@gmail.com]>
2021-08-08 10:45:42 -0700

    push

---
+++ ifelseifladderdemo/IfElseIfLadderDemo.java
@@ -0,0 +1,13 @@
+class IfElseIfLadderDemo{
+public static void main(String args[]){
+String trafficsignal="red";
+if(trafficsignal=="red"){
+System.out.println("please stop and wait");
+}
+else if(trafficsignal=="yellow"){
+System.out.println("please get ready to move");
+}
+else{
+System.out.println("you can move");}
+}
+}
