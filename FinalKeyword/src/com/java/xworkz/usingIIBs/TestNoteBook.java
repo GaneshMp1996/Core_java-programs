038df7c5234af7f1eaeb90c9b6e24da9ada871f4
ganeshmp1996 <[ganeshmpganesh3@gmail.com]>
2021-10-08 12:37:12 -0700

    PUSH

--- FinalKeyword/src/com/java/xworkz/usingIIBs/TestNoteBook.java
+++
@@ -1,9 +0,0 @@
-package com.java.xworkz.usingIIBs;
-
-public class TestNoteBook {
-	public static void main(String args[]) {
-		NoteBook sulekha = new NoteBook();
-		System.out.println(sulekha.NO_OF_PAGES);
-	}
-
-}
