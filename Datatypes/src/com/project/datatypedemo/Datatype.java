b8fced3f719b80927a9c2559283e552fbe3e2b77
Ganeshmp1996 <ganeshmpganesh3@gmail.com>
2022-09-26 16:46:06 +0530

    Push

---
+++ Datatypes/src/com/project/datatypedemo/Datatype.java
@@ -0,0 +1,30 @@
+package com.project.datatypedemo;
+
+public class Datatype {
+	public static void main(String[] args) {
+		byte byteValue=127;
+		boolean a=true;
+		char charValue='G';
+		int intValue=1234568;
+		short shortValue=25000;
+		float floatValue=1234.45f;
+		long cantactNumber=8548078524l;
+		double doubleValue=1.0e4d;
+		String name="Ganesh";
+		String allData="Ganesh867586.765464&^$^&&**";
+		
+		
+		System.out.println("The value of Byte is: "+byteValue);
+		System.out.println("The Value of Boolean is: "+a);
+		System.out.println("The value of Char is: "+charValue);
+		System.out.println("The value of Int is: "+intValue);
+		System.out.println("The value of Short is: "+shortValue);
+		System.out.println("The value of Float is: "+floatValue);
+		System.out.println("The value of long is: "+cantactNumber);
+		System.out.println("The value of Double is: "+doubleValue);
+		System.out.println("The value of Name is: "+name);
+		System.out.println("The value of AllData is: "+allData);
+		
+	}
+
+}
