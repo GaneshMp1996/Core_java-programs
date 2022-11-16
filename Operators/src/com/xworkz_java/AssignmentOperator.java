57dab1f39eca9c15dd7bec3bbcfad073fe8e85a6
Ganeshmp1996 <ganeshmpganesh3@gmail.com>
2022-11-16 12:30:42 +0530

    Push

---
+++ Operators/src/com/xworkz_java/AssignmentOperator.java
@@ -0,0 +1,13 @@
+package com.xworkz_java;
+
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		int a=30;
+		int b=50;
+		
+//		a=a+b;
+		a+=b;
+		System.out.println(a);
+	}
+
+}
